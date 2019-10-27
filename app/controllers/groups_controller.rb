dbbdc75ee07e26cf3ab671986038be0e98258902
oguraion <56718721+oguraion@users.noreply.github.com>
2019-10-27 18:55:41 +0900

    fixed

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,6 +27,7 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
   end
+  
   def set_group
     @group = Group.find(params[:id])
   end
